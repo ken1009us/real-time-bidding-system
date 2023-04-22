46efda747cfe97d908063ed24e9ec6427da06bf6
Ken Wu <kenwu1009us@gmail.com>
2023-04-21 23:18:57 -0700

    add bid class

---
+++ bid.py
@@ -0,0 +1,16 @@
+class Bid:
+    def __init__(self, item_id, username, price):
+        # TODO: Implement bid initialization code
+        pass
+
+    def get_item_id(self):
+        # TODO: Implement get_item_id method
+        pass
+
+    def get_username(self):
+        # TODO: Implement get_username method
+        pass
+
+    def get_price(self):
+        # TODO: Implement get_price method
+        pass
