887b05aab22d075233ecb00899ee02b1a69e4bfb
Ken Wu <kenwu1009us@gmail.com>
2023-04-21 23:19:12 -0700

    add db class

---
+++ db.py
@@ -0,0 +1,20 @@
+class Database:
+    def __init__(self, database_file):
+        # TODO: Implement database initialization code
+        pass
+
+    def add_user(self, username, password):
+        # TODO: Implement add_user method
+        pass
+
+    def authenticate_user(self, username, password):
+        # TODO: Implement authenticate_user method
+        pass
+
+    def add_bid(self, item_id, username, price):
+        # TODO: Implement add_bid method
+        pass
+
+    def get_highest_bid(self, item_id):
+        # TODO: Implement get_highest_bid method
+        pass
