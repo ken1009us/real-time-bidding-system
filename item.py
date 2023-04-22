9c3b8fad1821ffd9474886861e4fd137a712f070
Ken Wu <kenwu1009us@gmail.com>
2023-04-21 23:19:19 -0700

    add item class

---
+++ item.py
@@ -0,0 +1,16 @@
+class Item:
+    def __init__(self, item_id, item_name, starting_price):
+        # TODO: Implement item initialization code
+        pass
+
+    def get_id(self):
+        # TODO: Implement get_id method
+        pass
+
+    def get_name(self):
+        # TODO: Implement get_name method
+        pass
+
+    def get_starting_price(self):
+        # TODO: Implement get_starting_price method
+        pass
